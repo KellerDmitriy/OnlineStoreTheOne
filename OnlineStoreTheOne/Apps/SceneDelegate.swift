7fc6f791f70dc7099ba537eb83e3958edb2b8950
Razumov Pavel <k24b@bk.ru>
2024-04-16 14:58:11 +0300

    add Onboarding

--- OnlineStoreTheOne/Apps/SceneDelegate.swift
+++ OnlineStoreTheOne/Apps/SceneDelegate.swift
@@ -10,19 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        window = UIWindow(windowScene: windowScene)
-//        
-//        let tabBarController = TabBarController()
-//        let navigationController = UINavigationController(rootViewController: tabBarController)
-//        
-//        window?.rootViewController = navigationController
-//        window?.makeKeyAndVisible()
-//    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
