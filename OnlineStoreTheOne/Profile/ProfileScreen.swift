9bf46c4898516544b53d6b592b6fce28fffb12a7
dashyn <bolshakovadasha@gmail.com>
2024-04-19 20:39:42 +0300

    Update ProfileScreen

--- OnlineStoreTheOne/Profile/ProfileScreen.swift
+++ OnlineStoreTheOne/Profile/ProfileScreen.swift
@@ -9,112 +9,148 @@ import UIKit
 
 class ProfileScreen: UIViewController {
     
-    var profileImage = CreateImage(name: "ProfileImage")
-    var eyeButton = UIButton(primaryAction: passwordAction())
-    var userNameText = "dev@gmail.com"
-    var userName = createLabel(size: 16, font: "Inter-Bold", text: "Dev P", color: Colors.darkArsenic)
-    var userMail = createLabel(size: 14, font: "Inter-Bold", text: "dev@gmail.com", color: Colors.gray)
-    var userPassword = createLabel(size: 14, font: "Inter-Bold", text: "*****", color: Colors.gray)
-    var termsButton = createButton(action: action())
-    var typeButton = createButton(action: action())
-    var signOutButton = createButton(action: action())
-    
-    lazy var passwordStack: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.spacing = 4
-        stack.alignment = .leading
-        stack.distribution = .equalSpacing
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stack
+    //MARK: - UI elements
+    private lazy var profileImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "ProfileImage")
+        image.contentMode = .scaleToFill
+        image.layer.cornerRadius = 50
+        image.layer.masksToBounds = false
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
     }()
     
-    private lazy var textStack: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.spacing = 4
-        stack.alignment = .leading
-        stack.distribution = .fillProportionally
-        stack.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var editButton: UIButton = {
+        let button = UIButton(primaryAction: editImageAction())
+        button.setBackgroundImage(UIImage(named: "EditIcon"), for: .normal)
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = false
+        button.clipsToBounds = true
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 3
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
         
-        return stack
+    
+    private let userName = NewLabelFactory(text: "DevP", font: .bold, size: 16).createLabel()
+    
+    private lazy var userMail: UILabel = {
+        let label = NewLabelFactory(text: "dev@gmail.com", font: .light, size: 14).createLabel()
+        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+        let underlineAttributedString = NSAttributedString(string: label.text ?? "", attributes: underlineAttribute)
+        label.attributedText = underlineAttributedString
+        return label
+    }()
+    private lazy var termsButton: UIView = {
+        let button = ButtonLabelFactory(title: "Terms & Conditions", type: .standartButton, name: "ArrowIcon", homeView: view, action: termsAction(), textColor: nil).createButtonWithLabel()
+        return button
+    }()
+    
+    private lazy var typeButton: UIView = {
+        let button = ButtonLabelFactory(title: "Type of account", type: .standartButton, name: "ArrowIcon", homeView: view, action: action(), textColor: nil).createButtonWithLabel()
+        return button
+    }()
+    private lazy var signOutButton: UIView = {
+        let button = ButtonLabelFactory(title: "Sign Out", type: .standartButton, name: "SignOutIcon", homeView: view, action: signOutAction(), textColor: nil).createButtonWithLabel()
+        return button
     }()
     
     private lazy var btnStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 25
+        stack.addArrangedSubview(termsButton)
+        stack.addArrangedSubview(typeButton)
+        stack.addArrangedSubview(signOutButton)
         stack.translatesAutoresizingMaskIntoConstraints = false
-        
         return stack
     }()
+    
+
+    //MARK: - ViewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setImage()
-        setStack()
-        setButton()
+        setUpView()
+        setConstraint()
+    }
+    //MARK: - Actions
+    func termsAction() -> UIAction {
+        let act = UIAction { _ in
+            let vc = TermsConditionalsScreen()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        return act
+    }
+    //спросить про переход))))))
+    func signOutAction() -> UIAction {
+        let act = UIAction { _ in
+            let vc = OnboardingViewController()
+            self.navigationController?.popToRootViewController(animated: true)
+        }
+        return act
     }
     
-    func setImage() {
+    func editImageAction() -> UIAction {
+        let act = UIAction { _ in
+            let vc = EditImageScreen()
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
+        return act
+    }
+    func action() -> UIAction {
+        let action = UIAction { _ in
+            print("Profile")
+        }
+        return action
+    }
+}
+
+//MARK: - Extension
+private extension ProfileScreen {
+    
+    //MARK: - Set up view
+    func setUpView() {
+        view.backgroundColor = .white
+        
         view.addSubview(profileImage)
-        profileImage.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        profileImage.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        profileImage.layer.cornerRadius = 50
-        
-        profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 113).isActive = true
-        profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
-    }
-    
-    func setStack() {
-        eyeButton.setImage(UIImage(named: "EyeIcon"), for: .normal)
-        eyeButton.translatesAutoresizingMaskIntoConstraints = false
-        eyeButton.tintColor = Colors.gray
-        
-        eyeButton.heightAnchor.constraint(equalToConstant: 16).isActive = true
-        eyeButton.widthAnchor.constraint(equalToConstant: 16).isActive = true
-        
-        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
-        let underlineAttributedString = NSAttributedString(string: userMail.text ?? "", attributes: underlineAttribute)
-        userMail.attributedText = underlineAttributedString
-        
-        passwordStack.addArrangedSubview(eyeButton)
-        passwordStack.addArrangedSubview(userPassword)
-        
-        textStack.addArrangedSubview(userName)
-        textStack.addArrangedSubview(userMail)
-        textStack.addArrangedSubview(passwordStack)
-        
-        view.addSubview(textStack)
-        
-        NSLayoutConstraint.activate([
-            textStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 126),
-            textStack.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 20)
-            
-        ])
-        
-        
-        
-        
-        
-//        eyeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-//        eyeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50).isActive = true
-//        eyeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-//        eyeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 150).isActive = true
-    }
-    func setButton() {
-        btnStack.addArrangedSubview(typeButton)
-        btnStack.addArrangedSubview(termsButton)
-        btnStack.addArrangedSubview(signOutButton)
-        
+        view.addSubview(editButton)
+        view.addSubview(userName)
+        view.addSubview(userMail)
         view.addSubview(btnStack)
-        
+     
+    }
+    //MARK: - Set constraint
+    func setConstraint() {
         NSLayoutConstraint.activate([
+            profileImage.heightAnchor.constraint(equalToConstant: 100),
+            profileImage.widthAnchor.constraint(equalToConstant: 100),
+            profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 113),
+            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27),
+            
+            editButton.heightAnchor.constraint(equalToConstant: 32),
+            editButton.widthAnchor.constraint(equalToConstant: 32),
+            editButton.topAnchor.constraint(equalTo: profileImage.topAnchor, constant: 68),
+            editButton.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor, constant: 68),
+            
+            userName.topAnchor.constraint(equalTo: view.topAnchor, constant: 127),
+            userName.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 40),
+            
+            userMail.topAnchor.constraint(equalTo: userName.topAnchor, constant: 25),
+            userMail.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 40),
+            
             btnStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             btnStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             btnStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
         ])
     }
+    
+   
 }
+
+
